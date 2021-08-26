c6bbba4350785894a18b8af4a43e55d91f04b135
Liu, Jingzhen (Reigate) <jingzhen.liu@towerswatson.com>
2021-08-26 18:44:55 +0100

    Got required tickers of funds and added the functionality of sorting funds based on valuation measures.

--- PythonSQLPractice/PythonApplication1/DownloadFidelityData.py
+++ PythonSQLPractice/PythonApplication1/DownloadFidelityData.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import os
 
 def GetHistoricalDataArray(data):
     price=[]
@@ -9,6 +10,36 @@ def GetHistoricalDataArray(data):
         price.append(float(data[i]['Value']))
         date.append(data[i]['EndDate'])
     return price, date
+
+def SortFunds(mode,valuationmeasure):
+    #mode="FromLowestToHighest","FromHighestToLowest"
+    #valuationmeasure="earnings","sales","book","flow"
+    dir_path = os.path.dirname(os.getcwd())
+    filename = dir_path+'\\FidelityOver10YrTickers.json'  
+    with open(filename,'r') as file_object:  
+        data = json.loads(json.load(file_object)) 
+    isinList=data['ISINs']
+
+    ratioList={}
+   
+    for i in range(len(isinList)):
+        URL="https://www.fidelity.co.uk/factsheet-data/api/factsheet/"+isinList[str(i)]+"/portfolio"
+        r = requests.get(URL)
+        encodedText = r.text.encode("utf-8")
+        soup = BeautifulSoup(encodedText)
+        rows=soup.currentTag.currentTag.currentTag.currentTag.text
+        try:
+            valuations= json.loads(rows)['valuation']['fund']
+            if valuations[valuationmeasure]!="":
+                ratioList[isinList[str(i)]]=valuations[valuationmeasure]
+        except:
+            pass
+
+    if mode=="FromLowestToHighest":
+        newRatioList=sorted(ratioList.items(), key=lambda x: x[1])    
+    else:
+        newRatioList=sorted(ratioList.items(), key=lambda x: x[1],reverse=True)    
+    return newRatioList
 
 #Get SecId of the selected funds from Fidelity, the funds are ranked with recent 3-month performace
 URL = "https://lt.morningstar.com/api/rest.svc/9vehuxllxs/security/screener?page=1&pageSize=100&sortOrder=GBRReturnM3%20desc&outputType=json&version=1&languageId=en-GB&currencyId=GBP&universeIds=FOGBR%24%24ALL_3521&securityDataPoints=SecId%7CName%7CTenforeId%7CholdingTypeId%7Cisin%7Csedol%7CCustomAttributes1%7CCustomAttributes2%7CCustomExternalURL1%7CCustomExternalURL2%7CCustomExternalURL3%7CCustomIsClosed%7CCustomIsFavourite%7CCustomIsRecommended%7CCustomMarketCommentary%7CQR_MonthDate%7CExchangeId%7CExchangeCode%7CCurrency%7CLegalName%7CCustomBuyFee%7CYield_M12%7COngoingCostEstimated%7CCustomCategoryId3Name%7CStarRatingM255%7CQR_GBRReturnM12_5%7CQR_GBRReturnM12_4%7CQR_GBRReturnM12_3%7CQR_GBRReturnM12_2%7CQR_GBRReturnM12_1%7CCustomMinimumPurchaseAmount%7CCustomAdditionalBuyFee%7CCustomSellFee%7CTransactionFeeEstimated%7CPerformanceFee%7CGBRReturnM0%7CGBRReturnM12%7CGBRReturnM36%7CGBRReturnM60%7CGBRReturnM120%7CTrackRecordExtension&filters=&term=&subUniverseId=MFEI"
@@ -41,4 +72,5 @@ for i in range(len(IdList)):
         dictDate[NamesList[i]]=date
 
 
-print("Finished")
+
+
--- PythonSQLPractice/PythonApplication1/DownloadYahooData.py
+++ PythonSQLPractice/PythonApplication1/DownloadYahooData.py
@@ -3,12 +3,16 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import os 
 import json
-
+import pyodbc
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=server_name;'
+                      'Database=database_name;'
+                      'Trusted_Connection=yes;')
 dir_path = os.path.dirname(os.getcwd())
 filename = dir_path+'\\FTSE100Tickers.json'  
 with open(filename,'r') as file_object:  
     data = json.loads(json.load(file_object)) 
 
 Tickers=data['Tickers']
-
+tempTicker=Tickers['0']
 print('Finished')
